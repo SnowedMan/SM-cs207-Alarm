3c29d9b714c96cdeefed4b905174c7845e151c98
SnowedMan <i1g0rrrt@gmail.com>
2017-02-24 19:16:26 -0600

    Add files via upload

---
+++ Final.ino
@@ -0,0 +1,20 @@
+int input;
+int AnalogPin = A0;
+int piezoPin = 9;
+
+void setup() {
+Serial.begin(9600);  
+pinMode (piezoPin, OUTPUT);
+}
+
+void loop() {
+  input = analogRead(AnalogPin);
+  Serial.println(input);
+  if (input < 20)
+  {
+  analogWrite (piezoPin, 200);
+  delay(5000);
+  analogWrite (piezoPin,0);
+  }
+delay(500);
+}
